3558c10800655df09de09c609aa9659aae38523d
Jaeho Yi <akghxhs55@gmail.com>
2025-08-05 20:50:28 +0900

    ✨: Add shader type keyword completion

--- src/main/kotlin/kr/jaehoyi/gdshader/completion/GDShaderShaderTypeCompletionContributor.kt
+++ src/main/kotlin/kr/jaehoyi/gdshader/completion/GDShaderShaderTypeCompletionContributor.kt
@@ -9,11 +9,31 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.util.ProcessingContext
 import kr.jaehoyi.gdshader.GDShaderUtil
+import kr.jaehoyi.gdshader.psi.GDShaderFile
 import kr.jaehoyi.gdshader.psi.GDShaderShaderTypeName
 import kr.jaehoyi.gdshader.psi.GDShaderTypes
 
 class GDShaderShaderTypeCompletionContributor : CompletionContributor() {
     init {
+        extend(
+            CompletionType.BASIC,
+            PlatformPatterns.psiElement()
+                .withParent(GDShaderFile::class.java),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    result.addElement(LookupElementBuilder
+                        .create("shader_type")
+                        .withBoldness(true)
+                        .withTypeText("Keyword")
+                    )
+                }
+            }
+        )
+        
         extend(
             CompletionType.BASIC,
             PlatformPatterns.psiElement(GDShaderTypes.IDENTIFIER)
