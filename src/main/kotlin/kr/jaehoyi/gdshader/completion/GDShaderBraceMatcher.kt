7799374b6c8cb3870e9c33c40b7b02a0fa69915a
Jaeho Yi <akghxhs55@gmail.com>
2025-10-04 15:03:18 +0900

    ✨: Improve brace matching

--- src/main/kotlin/kr/jaehoyi/gdshader/completion/GDShaderBraceMatcher.kt
+++ src/main/kotlin/kr/jaehoyi/gdshader/completion/GDShaderBraceMatcher.kt
@@ -3,7 +3,9 @@ package kr.jaehoyi.gdshader.completion
 import com.intellij.lang.BracePair
 import com.intellij.lang.PairedBraceMatcher
 import com.intellij.psi.PsiFile
+import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
 import kr.jaehoyi.gdshader.psi.GDShaderTypes
 
 class GDShaderBraceMatcher : PairedBraceMatcher {
@@ -12,10 +14,21 @@ class GDShaderBraceMatcher : PairedBraceMatcher {
         BracePair(GDShaderTypes.CURLY_BRACKET_OPEN, GDShaderTypes.CURLY_BRACKET_CLOSE, true),
         BracePair(GDShaderTypes.BRACKET_OPEN, GDShaderTypes.BRACKET_CLOSE, false)
     )
+    
+    private val pairingAllowedTokenSet = TokenSet.create(
+        TokenType.WHITE_SPACE, GDShaderTypes.LINE_COMMENT, GDShaderTypes.BLOCK_COMMENT, GDShaderTypes.PP_DEFINE_LINE, 
+        GDShaderTypes.PP_UNDEF_LINE, GDShaderTypes.PP_ELSE_LINE, GDShaderTypes.PP_ELIF_LINE, 
+        GDShaderTypes.PP_ENDIF_LINE, GDShaderTypes.PP_IFDEF_LINE, GDShaderTypes.PP_IFNDEF_LINE, 
+        GDShaderTypes.PP_IF_LINE, GDShaderTypes.PP_ERROR_LINE, GDShaderTypes.PP_INCLUDE_LINE,
+        GDShaderTypes.PP_PRAGMA_LINE, GDShaderTypes.COMMA, GDShaderTypes.COLON, GDShaderTypes.SEMICOLON,
+        GDShaderTypes.PERIOD, GDShaderTypes.PARENTHESIS_CLOSE, GDShaderTypes.BRACKET_CLOSE,
+        GDShaderTypes.CURLY_BRACKET_CLOSE
+    )
 
     override fun getPairs(): Array<BracePair> = pairs
 
-    override fun isPairedBracesAllowedBeforeType(leftBraceType: IElementType, contextType: IElementType?): Boolean = true
+    override fun isPairedBracesAllowedBeforeType(leftBraceType: IElementType, contextType: IElementType?): Boolean =
+        contextType == null || pairingAllowedTokenSet.contains(contextType)
 
     override fun getCodeConstructStart(file: PsiFile, openingBraceOffset: Int): Int = openingBraceOffset
 }
