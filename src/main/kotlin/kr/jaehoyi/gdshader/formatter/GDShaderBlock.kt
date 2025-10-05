d63770b457550780a202fc9a6db5d6a2c28a1adc
Jaeho Yi <akghxhs55@gmail.com>
2025-10-05 16:29:35 +0900

    ✨: Improve indentation formatting

--- src/main/kotlin/kr/jaehoyi/gdshader/formatter/GDShaderBlock.kt
+++ src/main/kotlin/kr/jaehoyi/gdshader/formatter/GDShaderBlock.kt
@@ -16,18 +16,20 @@ class GDShaderBlock(
     override fun buildChildren(): MutableList<Block> {
         val blocks = mutableListOf<Block>()
         var child = myNode.firstChildNode
+        var isFirstChild = true
+        
         while (child != null) {
             if (child.text.isNotBlank()) {
-                val childIndent = calculateIndentFor(child)
-                
                 val block = GDShaderBlock(
                     child,
                     Wrap.createWrap(WrapType.NONE, false),
                     null,
                     spacingBuilder,
-                    childIndent
+                    calculateIndentFor(child, isFirstChild)
                 )
                 blocks.add(block)
+                
+                isFirstChild = false
             }
             child = child.treeNext
         }
@@ -40,7 +42,8 @@ class GDShaderBlock(
     override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
         val nodeType = myNode.elementType
         
-        if (nodeType in INDENT_TRIGGER_BLOCKS) {
+        if (nodeType in INDENT_TRIGGER_BLOCKS ||
+            nodeType in INDENT_TRIGGER_STATEMENTS) {
             return ChildAttributes(Indent.getNormalIndent(), null)
         }
         return ChildAttributes(Indent.getNoneIndent(), null)
@@ -52,18 +55,28 @@ class GDShaderBlock(
     override fun isLeaf(): Boolean 
         = myNode.firstChildNode == null
 
-    private fun calculateIndentFor(childNode: ASTNode) : Indent {
+    private fun calculateIndentFor(childNode: ASTNode, isFirstChild: Boolean) : Indent {
         val parentType = myNode.elementType
         val childType = childNode.elementType
         
         if (childType == GDShaderTypes.CURLY_BRACKET_OPEN ||
-            childType == GDShaderTypes.CURLY_BRACKET_CLOSE) {
+            childType == GDShaderTypes.CURLY_BRACKET_CLOSE ||
+            (parentType == GDShaderTypes.DO_WHILE_STATEMENT && childType == GDShaderTypes.CF_WHILE)) {
             return Indent.getNoneIndent()
         }
         
         if (parentType in INDENT_TRIGGER_BLOCKS) {
             return Indent.getNormalIndent()
         }
+        
+        val containsBlock = childNode.findChildByType(GDShaderTypes.BLOCK) != null
+        
+        if (parentType in INDENT_TRIGGER_STATEMENTS &&
+            !isFirstChild &&
+            !containsBlock) {
+            return Indent.getNormalIndent()
+        }
+        
         return Indent.getNoneIndent()
     }
 }
@@ -72,4 +85,21 @@ private val INDENT_TRIGGER_BLOCKS = TokenSet.create(
     GDShaderTypes.BLOCK,
     GDShaderTypes.STRUCT_BLOCK,
     GDShaderTypes.SWITCH_BLOCK
+)
+
+private val INDENT_TRIGGER_STATEMENTS = TokenSet.create(
+    GDShaderTypes.IF_STATEMENT,
+    GDShaderTypes.ELSE_CLAUSE,
+    GDShaderTypes.FOR_STATEMENT,
+    GDShaderTypes.WHILE_STATEMENT,
+    GDShaderTypes.DO_WHILE_STATEMENT,
+    GDShaderTypes.RETURN_STATEMENT,
+    GDShaderTypes.SHADER_TYPE_DECLARATION,
+    GDShaderTypes.RENDER_MODE_DECLARATION,
+    GDShaderTypes.STENCIL_MODE_DECLARATION,
+    GDShaderTypes.UNIFORM_GROUP_DECLARATION,
+    GDShaderTypes.UNIFORM_DECLARATION,
+    GDShaderTypes.CONSTANT_DECLARATION,
+    GDShaderTypes.VARYING_DECLARATION,
+    GDShaderTypes.LOCAL_VARIABLE_DECLARATION
 )
