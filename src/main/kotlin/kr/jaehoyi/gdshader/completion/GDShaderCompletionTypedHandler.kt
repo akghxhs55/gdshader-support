ca39ac5483f5aef0f40cf27f8dd01ea53508e17b
Jaeho Yi <akghxhs55@gmail.com>
2025-11-30 15:30:34 +0900

    ✨: Add completion typed handler

---
+++ src/main/kotlin/kr/jaehoyi/gdshader/completion/GDShaderCompletionTypedHandler.kt
@@ -0,0 +1,37 @@
+package kr.jaehoyi.gdshader.completion
+
+import com.intellij.codeInsight.AutoPopupController
+import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import kr.jaehoyi.gdshader.psi.GDShaderFile
+
+class GDShaderCompletionTypedHandler : TypedHandlerDelegate() {
+
+    override fun checkAutoPopup(charTyped: Char, project: Project, editor: Editor, file: PsiFile): Result {
+        if (file !is GDShaderFile) {
+            return Result.CONTINUE
+        }
+        
+        when (charTyped) {
+            ',' -> AutoPopupController.getInstance(project).autoPopupMemberLookup(editor, null)
+        
+            ' ' -> {
+                val offset = editor.caretModel.offset
+                if (offset < 2) {
+                    return Result.CONTINUE
+                }
+                
+                val document = editor.document
+                val prevChar = document.charsSequence[offset - 1]
+                if (prevChar == ',') {
+                    AutoPopupController.getInstance(project).autoPopupMemberLookup(editor, null)
+                }
+            }
+        }
+        
+        return Result.CONTINUE
+    }
+    
+}
