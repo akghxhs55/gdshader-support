cd8e67aea8df6671b767af57dbcdeeaf6d972d68
Jaeho Yi <akghxhs55@gmail.com>
2026-01-17 16:15:39 +0900

    ✨: Add quote handler inside include preprocessor

--- src/main/kotlin/kr/jaehoyi/gdshader/editor/GdsQuoteHandler.kt
+++ src/main/kotlin/kr/jaehoyi/gdshader/editor/GdsQuoteHandler.kt
@@ -1,9 +1,52 @@
 package kr.jaehoyi.gdshader.editor
 
 import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.highlighter.HighlighterIterator
 import kr.jaehoyi.gdshader.psi.GdsTypes
 
 class GdsQuoteHandler : SimpleTokenSetQuoteHandler(
     GdsTypes.STRING_CONSTANT,
     GdsTypes.UNTERMINATED_STRING_CONSTANT
-)
+) {
+
+    override fun isOpeningQuote(iterator: HighlighterIterator?, offset: Int): Boolean {
+        if (super.isOpeningQuote(iterator, offset)) return true
+        if (iterator == null) return false
+
+        if (iterator.tokenType == GdsTypes.PP_INCLUDE_LINE) {
+            val doc = iterator.document ?: return false
+            val start = iterator.start
+
+            if (offset > start) {
+                val textBefore = doc.charsSequence.subSequence(start, offset).toString().trim()
+                if (textBefore.endsWith("#include")) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    override fun hasNonClosedLiteral(editor: Editor?, iterator: HighlighterIterator?, offset: Int): Boolean {
+        if (iterator != null && iterator.tokenType == GdsTypes.PP_INCLUDE_LINE) {
+            return true
+        }
+
+        return super.hasNonClosedLiteral(editor, iterator, offset)
+    }
+
+    override fun isClosingQuote(iterator: HighlighterIterator?, offset: Int): Boolean {
+        if (super.isClosingQuote(iterator, offset)) return true
+        if (iterator == null) return false
+
+        if (iterator.tokenType == GdsTypes.PP_INCLUDE_LINE) {
+            val doc = iterator.document ?: return false
+            if (offset < doc.textLength && doc.charsSequence[offset] == '"') {
+                return true
+            }
+        }
+        return false
+    }
+    
+}
