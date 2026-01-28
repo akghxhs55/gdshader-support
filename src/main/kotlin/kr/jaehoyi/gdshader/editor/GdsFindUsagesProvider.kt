81c823212debf2a40ca7432af7f23b5b17cda9c7
Jaeho Yi <akghxhs55@gmail.com>
2026-01-28 22:52:43 +0900

    ✨: Add find usages for struct member

--- src/main/kotlin/kr/jaehoyi/gdshader/editor/GdsFindUsagesProvider.kt
+++ src/main/kotlin/kr/jaehoyi/gdshader/editor/GdsFindUsagesProvider.kt
@@ -7,6 +7,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.TokenSet
 import kr.jaehoyi.gdshader.GdsLexerAdapter
 import kr.jaehoyi.gdshader.psi.GdsFunctionNameDecl
+import kr.jaehoyi.gdshader.psi.GdsStructMemberNameDecl
 import kr.jaehoyi.gdshader.psi.GdsStructNameDecl
 import kr.jaehoyi.gdshader.psi.GdsTokenSets
 import kr.jaehoyi.gdshader.psi.GdsTypes
@@ -27,7 +28,8 @@ class GdsFindUsagesProvider : FindUsagesProvider {
     override fun canFindUsagesFor(element: PsiElement): Boolean {
         return element is GdsVariableNameDecl ||
                 element is GdsFunctionNameDecl ||
-                element is GdsStructNameDecl
+                element is GdsStructNameDecl ||
+                element is GdsStructMemberNameDecl
     }
 
     override fun getHelpId(element: PsiElement): @NonNls String? = null
@@ -37,6 +39,7 @@ class GdsFindUsagesProvider : FindUsagesProvider {
             is GdsVariableNameDecl -> "Variable"
             is GdsFunctionNameDecl -> "Function"
             is GdsStructNameDecl -> "Struct"
+            is GdsStructMemberNameDecl -> "Struct Member"
             else -> ""
         }
 
@@ -45,6 +48,7 @@ class GdsFindUsagesProvider : FindUsagesProvider {
             is GdsVariableNameDecl -> element.name
             is GdsFunctionNameDecl -> element.name
             is GdsStructNameDecl -> element.name
+            is GdsStructMemberNameDecl -> element.name
             else -> ""
         }
 
