7ad25d15dd7108ca0e082d33644d5bb96bcad51a
Jaeho Yi <akghxhs55@gmail.com>
2026-03-22 21:50:37 +0900

    🐛: Fix plugin exception

--- src/main/kotlin/kr/jaehoyi/gdshader/formatter/GdsCodeStyleSettingsProvider.kt
+++ src/main/kotlin/kr/jaehoyi/gdshader/formatter/GdsCodeStyleSettingsProvider.kt
@@ -3,11 +3,15 @@ package kr.jaehoyi.gdshader.formatter
 import com.intellij.application.options.CodeStyleAbstractConfigurable
 import com.intellij.application.options.CodeStyleAbstractPanel
 import com.intellij.psi.codeStyle.CodeStyleConfigurable
+import com.intellij.lang.Language
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.codeStyle.CodeStyleSettingsProvider
+import kr.jaehoyi.gdshader.GdsLanguage
 
 class GdsCodeStyleSettingsProvider : CodeStyleSettingsProvider() {
-    
+
+    override fun getLanguage(): Language = GdsLanguage
+
     override fun createCustomSettings(settings: CodeStyleSettings) =
         GdsCodeStyleSettings(settings)
 
